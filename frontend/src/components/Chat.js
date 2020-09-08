2594f6b05722dc3d42f5f211cf5e358c0c22a1b0
ffthkrdg <ffthkrdg@gmail.com>
2020-09-08 13:23:43 +0300

    components created

---
+++ frontend/src/components/Chat.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Chat.css'
+function Chat() {
+  return <div>I am chat</div>
+}
+
+export default Chat
---
+++ frontend/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyACLdbWc4NvAkKiUJxbYz9lf08VrBGgDlg',
+  authDomain: 'whatsapp-clone-88251.firebaseapp.com',
+  databaseURL: 'https://whatsapp-clone-88251.firebaseio.com',
+  projectId: 'whatsapp-clone-88251',
+  storageBucket: 'whatsapp-clone-88251.appspot.com',
+  messagingSenderId: '452838000726',
+  appId: '1:452838000726:web:47a92d858579a9ca2ed3f7',
+  measurementId: 'G-HWCH45K1DD'
+}

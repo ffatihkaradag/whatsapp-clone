91410139c7e9f1f78ed1b43fd491f170d9ee59b2
ffthkrdg <ffthkrdg@gmail.com>
2020-09-09 09:59:57 +0300

    pusher created

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,94 @@
-import express from "express";
-import mongoose from "mongoose";
+import express from 'express'
+import mongoose from 'mongoose'
+import Pusher from 'pusher'
+import Messages from './dbMessages.js'
 
 //app config
-const app = express();
-const port = process.env.PORT || 9000;
+const app = express()
+const port = process.env.PORT || 9000
+
+//pusher
+var pusher = new Pusher({
+  appId: '1069662',
+  key: '81dd554cf4226a3d6393',
+  secret: '304029951c5e16da68fb',
+  cluster: 'eu',
+  encrypted: true
+})
+
+pusher.trigger('my-channel', 'my-event', {
+  message: 'hello world'
+})
 
 //middlewares
-app.use(express.json());
+app.use(express.json())
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"),
-    res.setHeader("Access-Control-Allow-Headers", "*"),
-    next();
-});
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  next()
+})
 
 //DB config
 const connection_url =
-  "mongodb+srv://admin:FMWxdmiBUbVOfLPd@cluster0.xut2l.mongodb.net/whatsapp?retryWrites=true&w=majority";
+  'mongodb+srv://admin:FMWxdmiBUbVOfLPd@cluster0.xut2l.mongodb.net/whatsapp?retryWrites=true&w=majority'
 
 mongoose.connect(connection_url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+  useCreateIndex: true
+})
+
+const db = mongoose.connection
+
+db.once('open', () => {
+  console.log('db connected')
+
+  const msgCollection = db.collection('messagecontents')
+  const changeStream = msgCollection.watch()
+
+  changeStream.on('change', (change) => {
+    if (change.operationType === 'insert') {
+      const messageDetails = change.fullDocument
+      pusher.trigger('messages', 'inserted', {
+        name: messageDetails.name,
+        message: messageDetails.message
+      })
+    } else {
+      console.log('error trigger pusher')
+    }
+  })
+})
 
 //api endpoints
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// get all messages
+app.get('/messages/sync', (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(200).send(data)
+    }
+  })
+})
+
+// create a new message
+app.post('/messages/new', (req, res) => {
+  const dbMessage = req.body
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(201).send(data)
+    }
+  })
+})
 
 //listen
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
-});
+  console.log(`Listening at http://localhost:${port}`)
+})
